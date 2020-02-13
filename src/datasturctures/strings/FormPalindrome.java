3ea8751b62fe008d0984f3ef87438d7358c9e92f
Robin Singh <Robin.Singh1@wdc.com>
2020-02-13 15:49:37 +0530

    commit

--- src/datasturctures/strings/FormPalindrome.java
+++ src/datasturctures/strings/FormPalindrome.java
@@ -9,17 +9,21 @@ public class FormPalindrome {
 	 * */
 	
 	static int formPalindrome(String s, int fi, int li) {
+		//this will apply in last stack when both indexes merge
 		if(fi==li)
 			return 0;	
+		//this will apply when two characters r remaining
 		if(fi==li-1) {		//eg ab -- solution -> bab or aba
 			if(s.charAt(fi)==s.charAt(li))
 				return 0;
 			else
 				return 1;
 		}
+		//if chars at indexes are equal check for remaining string
 		if(s.charAt(fi)==s.charAt(li)) {
 			return formPalindrome(s, fi+1, li-1);
 		}else {
+			//else min of (either keep right index or keep left index). +1 because add
 			return Math.min(formPalindrome(s, fi+1, li), formPalindrome(s, fi, li-1))+1;
 		}
 		
